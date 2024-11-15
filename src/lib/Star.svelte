a391009feed6b490278ee8dfc44b6cfd502f25f8
Hector <hector@hjs.dev>
2024-11-15 22:34:29 +0000

    base implementation

---
+++ src/lib/Star.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let textSize;
+
+  const x = Math.random() * 100
+  const y = Math.random() * 60
+  // 300 characters becomes 15px
+  const size = Math.max(1, Math.floor(textSize / 20))
+</script>
+
+<div class="star"
+     style="left: {x}%; top: {y}%; width: {size}px; height: {size}px;">
+</div>
+
+<style>
+  .star {
+    position: fixed;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 5px 1px rgba(255, 255, 255, 0.6);
+  }
+</style>
---
+++ src/lib/bsky.ts
@@ -0,0 +1,52 @@
+//import { Jetstream } from "@skyware/jetstream";
+//import Websocket from "ws";
+
+
+
+
+class Stream {
+  private _jetstreamUrls = [
+    "wss://jetstream1.us-east.bsky.network/subscribe",
+    "wss://jetstream2.us-east.bsky.network/subscribe",
+    "wss://jetstream3.us-east.bsky.network/subscribe",
+    "wss://jetstream4.us-east.bsky.network/subscribe"
+  ]
+  private _rawStream: WebSocket | null = null
+  private _started: boolean = false
+
+  private randomUrl(): string {
+    const jetstreamUrl = this._jetstreamUrls[Math.floor(Math.random() * this._jetstreamUrls.length)];
+    console.log(`connecting to: ${jetstreamUrl}`)
+    return jetstreamUrl
+  }
+
+  private setUpStream(handler: (payload: any) => void) {
+    this._rawStream =  new WebSocket(this.randomUrl());
+    this._rawStream.onmessage = handler;
+    this._rawStream.onclose = () => {
+      console.log("connection closed");
+      if (this._started) {
+        this.setUpStream(handler);
+      }
+    }
+  }
+
+  connect(handler: (payload: any) => void) {
+    if (this._rawStream) {
+      throw new Error("stream already connected");
+    }
+
+    this._started = true;
+    this.setUpStream(handler);
+  }
+
+  disconnect() {
+    if (this._rawStream) {
+      this._started = false;
+      this._rawStream.close();
+      this._rawStream = null;
+    }
+  }
+}
+
+export const stream = new Stream();
