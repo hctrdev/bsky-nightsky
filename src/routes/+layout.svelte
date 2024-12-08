7fa8c3ad2f2da5d8252f18f0c4cae2f302c52245
Hector <hector@hjs.dev>
2024-12-08 20:29:36 +0000

    use store to check posted user

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,6 @@
 
   let stars: StarData[] = [];
   let specialStars: StarData[] = [];
-  let specialDid: string;
 
   const maxStarsAtOnce = 150;
   const addStar = (data: PostData) => {
@@ -32,7 +31,7 @@
       size: data.postText.length,
       url: url,
     };
-    if (data.userDid === specialDid) {
+    if (data.userDid === $specialProfile.did) {
       specialStars = [...specialStars, star];
     } else {
       stars = [...stars, star];
@@ -45,7 +44,6 @@
 
     if (userHandle) {
       getProfile(userHandle).then((profile) => {
-        specialDid = profile?.did ?? '';
         specialProfile.set(profile);
       });
     }
