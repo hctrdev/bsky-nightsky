4dab794dfd17057429a435c0d5ee7f80f5c135e3
Hector <hector@hjs.dev>
2024-12-09 19:42:09 +0000

    fmt

--- src/lib/bskyProfile.ts
+++ src/lib/bskyProfile.ts
@@ -8,13 +8,12 @@ export interface ProfileData {
 
 export const getProfile = async (actor: string): Promise<null | ProfileData> => {
   const cleanActor = actor.replace('@', '').trim();
-  return fetch(`${API_URL}/xrpc/app.bsky.actor.getProfile?actor=${cleanActor}`)
-  .then((res) => {
+  return fetch(`${API_URL}/xrpc/app.bsky.actor.getProfile?actor=${cleanActor}`).then((res) => {
     if (res.ok) {
       return res.json();
     } else {
-      console.error("error fetching profile", res);
+      console.error('error fetching profile', res);
       return null;
-    }}
-  );
+    }
+  });
 };
